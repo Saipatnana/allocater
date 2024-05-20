3bcb653e8467149df8d5d655f24ae5eb5bfd3676
saipatnana524 <saipatnana524@gmail.com>
2024-05-20 13:50:51 +0000

    footer is added

--- src/components/Pdf.jsx
+++ src/components/Pdf.jsx
@@ -179,6 +179,7 @@ const Pdf = (props) => {
               </tbody>
             </table>
           </div>
+          <p className="footer">Developed by Sai Patnana</p>
         </div>
         <div className="btn-container">
           <button className="download-btn" onClick={() => generatePDF(getTargetElement, options)}>
